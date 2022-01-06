f16414556245571d2c2c80c29d8ca64b1f51957b
Poulomi Das <poulomidas976@gmail.com>
2022-01-06 20:35:49 +0530

    First Commit

---
+++ src/dynamicProgramming/ChallengeLeetCode/ClimbingStairs.java
@@ -0,0 +1,17 @@
+package dynamicProgramming.ChallengeLeetCode;
+
+public class ClimbingStairs {
+
+  public static void main(String[] args) {
+    ClimbingStairs climbingStairs = new ClimbingStairs();
+    int numberOfSteps = 4;
+    int[] ways = {1, 2};
+
+    int numberOfWays = climbingStairs.findNumberOfWays(numberOfSteps, ways);
+  }
+
+  private int findNumberOfWays(int numberOfSteps, int[] ways) {
+
+    int[] numberOfWays = new int[numberOfSteps + 1]
+  }
+}
---
+++ src/dynamicProgramming/ChallengeLeetCode/FibonacciNumber.java
@@ -0,0 +1,6 @@
+package dynamicProgramming.ChallengeLeetCode;
+
+public class FibonacciNumber {
+
+
+}
